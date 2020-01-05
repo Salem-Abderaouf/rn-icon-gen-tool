cc6af02593f9e1958f58c143a1cbd028452b0a2d
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-05 18:54:21 +0100

    code optimize

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 window.addEventListener('load', function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
         if (this.files && this.files[0]) {
-            var img = document.getElementById('myImg');  
+            let img = document.getElementById('image');  
             img.src = URL.createObjectURL(this.files[0]); 
             img.onload = imageIsLoaded;
         }
     });
   });
 function imageIsLoaded() { 
-    var image = { 
+    let image = { 
         src:this.src,
         sizes : [48,72,96,144,192],
         roundness : ['8px','50%']
 } 
-var {src , roundness} = image
-image.sizes.forEach(size =>{
-    var radiusImageElem = document.createElement('img')
-    var roundedImageElem = document.createElement('img')
+let {src , roundness , sizes} = image;
+    sizes.forEach(size =>{
+    let radiusImageElem = document.createElement('img')
+    let roundedImageElem = document.createElement('img')
     radiusImageElem.src = src;roundedImageElem.src = src;
     radiusImageElem.height = size;radiusImageElem.width = size;
     roundedImageElem.height = size;roundedImageElem.width = size;
     radiusImageElem.style.borderRadius = roundness[0];
     roundedImageElem.style.borderRadius = roundness[1];
     radiusImageElem.classList.add('radiusImage')
-    roundedImageElem.classList.add('roundImage')
     document.body.appendChild(roundedImageElem)
     document.body.appendChild(radiusImageElem)
     })
-//setup file system and export it zip
+//setup it into file system and export it zip
+}
+
+function create_zip() {
+    var zip = new JSZip();
+	zip.add("folder/hello1.txt", "Hello First World\n");
+	zip.add("hello2.txt", "Hello Second World\n");
+	content = zip.generate();
+	location.href="data:application/zip;base64," + content;
 }
