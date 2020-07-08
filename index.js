d675c258c0ebfec9af5f768a8f29be15f17b950a
Salem-Abderaouf <salamasalem84@gmail.com>
2020-07-08 08:34:22 +0100

    Update index.js file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import sharp from 'sharp';
 import fs from 'fs';
 
-const inputPath = "/root/Documents/work/rn-tool/AlexandraElbakyan.jpg";
+const args = process.argv;
+//const inputPath = "/root/Documents/work/rn-tool/AlexandraElbakyan.jpg";
+const inputPath = args[2]
 const props = [{
     'mipmap-hdpi': 48
   },
@@ -19,11 +21,35 @@ const props = [{
   }
 ]
 const generateImages = (prop) => {
+  const rect = new Buffer.from(
+    `<svg><rect x="0" y="0" width="${Number(Object.values(prop))}" height="${Number(Object.values(prop))}" rx="8" ry="8"/></svg>`
+  );
+  const round = new Buffer.from(
+    `<svg><rect x="0" y="0" width="${Number(Object.values(prop))}" height="${Number(Object.values(prop))}" rx="50%" ry="50%"/></svg>`
+  );
+  // resize to 8px border-radius image
   sharp(inputPath)
     .resize(Number(Object.values(prop)), Number(Object.values(prop)), {
-      fit: "contain"
+      fit: "cover"
     })
+    .composite([{
+      input: rect,
+      blend: 'dest-in'
+    }])
     .toFile(`./${Object.keys(prop)[0]}/ic_launcher.png`)
+    .then(() => {
+      console.log(`${Object.values(prop)[0]}px Image Saved at ${Object.keys(prop)[0]}`);
+    });
+  // resize to rounded image
+  sharp(inputPath)
+    .resize(Number(Object.values(prop)), Number(Object.values(prop)), {
+      fit: "cover"
+    })
+    .composite([{
+      input: round,
+      blend: 'dest-in'
+    }])
+    .toFile(`./${Object.keys(prop)[0]}/ic_launcher_rounded.png`)
     .then(() => {
       console.log(`${Object.values(prop)[0]}px Image Saved at ${Object.keys(prop)[0]}`);
     });
